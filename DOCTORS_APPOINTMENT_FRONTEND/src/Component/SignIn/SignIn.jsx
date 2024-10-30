33edf85b8a643cd0af93312eeed3670e893634d0
oluwabukunmi Akintola <oluwabukunmiakintola@gmail.com>
2024-10-30 07:19:32 +0000

    signin up

--- DOCTORS_APPOINTMENT_FRONTEND/src/Component/SignIn/SignIn.jsx
+++ DOCTORS_APPOINTMENT_FRONTEND/src/Component/SignIn/SignIn.jsx
@@ -15,7 +15,7 @@ const validationSchema = yup.object({
 });
 
 const SignIn = () => {
-  const url = "https://hospital-ooo.vercel.app/user/sigin";
+  const url = "https://hospital-ooo.vercel.app/user/signin";
   const navigate = useNavigate();
   
   const formik = useFormik({
